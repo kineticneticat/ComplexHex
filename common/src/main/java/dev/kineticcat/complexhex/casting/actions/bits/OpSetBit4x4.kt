787c45bff0e357036c43efafc4a66c67a1aefe77
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2026-05-08 19:48:47 +0100

    set matrix now takes 3x3, 3x4, 4x3, & 4x4 matrices, and updated the docs to match

--- common/src/main/java/dev/kineticcat/complexhex/casting/actions/bits/OpSetBit4x4.kt
+++ common/src/main/java/dev/kineticcat/complexhex/casting/actions/bits/OpSetBit4x4.kt
@@ -14,6 +14,7 @@ import dev.kineticcat.complexhex.api.toMatrix4f
 import dev.kineticcat.complexhex.mixin.BITInvokers.DisplayInvoker
 import net.minecraft.world.entity.Display
 import org.jblas.DoubleMatrix
+import org.jblas.ranges.IntervalRange
 import ram.talia.moreiotas.api.getMatrix
 
 object OpSetBit4x4 : SpellAction {
@@ -21,10 +22,19 @@ object OpSetBit4x4 : SpellAction {
 
     override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
         val e = args.getEntity(0)
-        val mat = args.getMatrix(1)
+        var mat = args.getMatrix(1)
         env.assertEntityInRange(e)
         if (e !is Display) throw MishapBadEntity.of(e, "bit")
-        if (!(mat.rows == 4 && mat.columns == 4)) throw MishapInvalidIota.of(args[1], 0, "4x4matrix")
+        if (!(mat.rows == 4 && mat.columns == 4))
+
+        mat = when (mat.rows to mat.columns) {
+            3 to 3 -> DoubleMatrix.eye(4).put(IntervalRange(0,3), IntervalRange(0,3), mat)
+            3 to 4 -> DoubleMatrix.eye(4).put(IntervalRange(0,3), IntervalRange(0,4), mat)
+            4 to 3 -> DoubleMatrix.eye(4).put(IntervalRange(0,4), IntervalRange(0,3), mat)
+            4 to 4 -> mat
+            else -> throw MishapInvalidIota.of(args[1], 0, "transformationmatrix")
+        }
+
         return SpellAction.Result(
             Spell(e, mat),
             MediaConstants.DUST_UNIT,
